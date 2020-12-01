08f0520c6c1649302f43f65df7542bf0fd7bec8e
frmauro <frmauro8@gmail.com>
2020-12-01 12:04:58 -0300

    implementation swagger api doc

--- src/main/java/com/quark/salesorder/config/SwaggerConfig.java
+++ src/main/java/com/quark/salesorder/config/SwaggerConfig.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.PathSelectors;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig  {
+public class SwaggerConfig {
    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
