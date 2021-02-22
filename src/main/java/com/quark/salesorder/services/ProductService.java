4cc4261a8538f83b9fb5deae141c9001537b6d15
frmauro <frmauro8@gmail.com>
2021-02-22 10:01:11 -0300

    implementation ProductService.java

--- src/main/java/com/quark/salesorder/services/ProductService.java
+++ src/main/java/com/quark/salesorder/services/ProductService.java
@@ -1,8 +1,17 @@
 package com.quark.salesorder.services;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private ProductService(){
+    }
+
+    private static final HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
+    private static final String serviceURL = "http://localhost:8087/Product/";
     
 }
