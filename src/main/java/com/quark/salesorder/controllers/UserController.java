14d7aaccff10cc52cce8b64c54cd4cc8ea728734
frmauro <frmauro8@gmail.com>
2021-04-07 11:59:32 -0300

    create userController

--- src/main/java/com/quark/salesorder/controllers/UserController.java
+++ src/main/java/com/quark/salesorder/controllers/UserController.java
@@ -1,5 +1,27 @@
 package com.quark.salesorder.controllers;
 
+import com.quark.salesorder.dtos.UserDto;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+
+    @CrossOrigin
+    @PostMapping
+    public ResponseEntity<String> updateAmount(@RequestBody UserDto dto)
+    {
+        String result = dto.getEmail();
+        //dtosJson = JSONUtils.covertFromObjectToJson(dtos);
+        //var result = ProductService.updateAmount(dtosJson);
+        return ResponseEntity.ok().body(result);
+    }
     
 }
---
+++ src/main/java/com/quark/salesorder/dtos/UserDto.java
@@ -0,0 +1,49 @@
+package com.quark.salesorder.dtos;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String email;
+    private String password;
+    private String token;
+
+    public UserDto(){}
+
+    public UserDto(String id, String email, String password, String token) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+
+}
