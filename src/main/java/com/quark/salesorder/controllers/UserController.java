0f1ce06c2e6db8e5fa6b9e02beefc46fd94e123f
frmauro <frmauro8@gmail.com>
2021-04-08 11:28:13 -0300

    create userService

--- src/main/java/com/quark/salesorder/controllers/UserController.java
+++ src/main/java/com/quark/salesorder/controllers/UserController.java
@@ -1,6 +1,13 @@
 package com.quark.salesorder.controllers;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.quark.salesorder.dtos.UserDto;
+import com.quark.salesorder.services.UserService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,11 +23,13 @@ public class UserController {
 
     @CrossOrigin
     @PostMapping
-    public ResponseEntity<String> updateAmount(@RequestBody UserDto dto)
+    public ResponseEntity<String> getByEmailAndPassword(@RequestBody UserDto dto) 
+        throws JsonMappingException, KeyManagementException, NoSuchAlgorithmException, 
+               InterruptedException, ExecutionException, IOException
     {
-        String result = dto.getEmail();
-        //dtosJson = JSONUtils.covertFromObjectToJson(dtos);
-        //var result = ProductService.updateAmount(dtosJson);
+        String email = dto.getEmail();
+        String password = dto.getPassword();
+        var result = UserService.getByEmailAndPassword(email, password);
         return ResponseEntity.ok().body(result);
     }
     
