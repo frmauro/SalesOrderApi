de2b4b8854e62afe12419fbf83c2dcc2b459ec64
frmauro <frmauro8@gmail.com>
2021-06-18 13:02:02 -0300

    implement grpc comunicattion

---
+++ src/main/java/com/quark/salesorder/ServiceGRPC/ProductService.java
@@ -0,0 +1,21 @@
+package com.quark.salesorder.ServiceGRPC;
+
+//import io.grpc.ManagedChannel;
+//import io.grpc.ManagedChannelBuilder;
+
+//import com.grpcserver.grpcserver.HelloRequest;
+//import com.grpcserver.grpcserver.HelloReply;
+//import com.grpcserver.grpcserver.GrpcserverServiceGrpc;
+
+public class ProductService {
+    
+
+    //ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8030).usePlaintext().build();
+
+    //GrpcserverServiceGrpc.GrpcserverServiceBlockingStub stub = GrpcserverServiceGrpc.newBlockingStub(channel);
+
+    //HelloReply helloReply = stub.sayHello(HelloRequest.newBuilder().setName("Francisco").build());
+
+    //channel.shutdown();
+
+}
