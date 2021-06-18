cca6841255c01eae8f6f0e5e37f0cbe98edc94fe
frmauro <frmauro8@gmail.com>
2021-06-18 15:52:49 -0300

    implement grpc comunicattion

--- src/main/java/com/quark/salesorder/ServiceGRPC/ProductService.java
+++ src/main/java/com/quark/salesorder/ServiceGRPC/ProductService.java
@@ -1,21 +1,23 @@
 package com.quark.salesorder.ServiceGRPC;
 
-//import io.grpc.ManagedChannel;
-//import io.grpc.ManagedChannelBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import com.grpcserver.grpcserver.HelloRequest;
-//import com.grpcserver.grpcserver.HelloReply;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import com.quark.salesorder.ServiceGRPC.ProductService;
+//import com.quark.salesorder.ProductServiceProtoGrpc.ProductRequest;
 //import com.grpcserver.grpcserver.GrpcserverServiceGrpc;
 
 public class ProductService {
     
+    public static String getAllProducts() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8030).usePlaintext().build();
+        //ProductService.ProductServiceBlockingStub stub = ProductService.newBlockingStub(channel);
+        //HelloReply helloReply = stub.sayHello(HelloRequest.newBuilder().setName("Francisco").build());
+        channel.shutdown();
+        return "OK";
+    }
 
-    //ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8030).usePlaintext().build();
-
-    //GrpcserverServiceGrpc.GrpcserverServiceBlockingStub stub = GrpcserverServiceGrpc.newBlockingStub(channel);
-
-    //HelloReply helloReply = stub.sayHello(HelloRequest.newBuilder().setName("Francisco").build());
-
-    //channel.shutdown();
 
 }
