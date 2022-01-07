8ce95b64de3e4b9920f7395d8693203f6212e5de
frmauro <frmauro8@gmail.com>
2022-01-07 16:43:29 -0300

    many changes

--- src/main/java/com/quark/salesorder/ServiceGRPC/OrderServiceGRPC.java
+++ src/main/java/com/quark/salesorder/ServiceGRPC/OrderServiceGRPC.java
@@ -2,7 +2,29 @@ package com.quark.salesorder.ServiceGRPC;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class OrderServiceGRPC {
+import SalesOrderApi.Empty;
+import SalesOrderApi.ItemResponse;
+import SalesOrderApi.OrderReply;
+import SalesOrderApi.OrderRequest;
+import SalesOrderApi.OrderServiceProtoGrpc.OrderServiceProtoImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class OrderServiceGRPC extends SalesOrderApi.OrderServiceProtoGrpc.OrderServiceProtoImplBase  {
     
+     public void SendOrder(OrderRequest request, StreamObserver<OrderReply> responseObserver) {
+        Integer id = 2;
+        OrderReply reply = OrderReply.newBuilder().setId(id).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+     }
+
+     public void GetOrders(Empty request, StreamObserver<ItemResponse> responseObserver) {
+        ItemResponse reply = ItemResponse.newBuilder().build();
+        
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+     }
+
 }
