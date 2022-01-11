777c6de6e070fca38f1ab55846772d136db73908
frmauro <frmauro8@gmail.com>
2022-01-11 12:08:20 -0300

    many changes

--- src/main/java/com/quark/salesorder/ServiceGRPC/OrderServiceGRPC.java
+++ src/main/java/com/quark/salesorder/ServiceGRPC/OrderServiceGRPC.java
@@ -1,11 +1,14 @@
 package com.quark.salesorder.ServiceGRPC;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import SalesOrderApi.Empty;
 import SalesOrderApi.ItemResponse;
 import SalesOrderApi.OrderReply;
 import SalesOrderApi.OrderRequest;
+import SalesOrderApi.OrderResponse;
 import SalesOrderApi.OrderServiceProtoGrpc.OrderServiceProtoImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -13,18 +16,29 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 public class OrderServiceGRPC extends SalesOrderApi.OrderServiceProtoGrpc.OrderServiceProtoImplBase  {
     
-     public void SendOrder(OrderRequest request, StreamObserver<OrderReply> responseObserver) {
+     public void SendOrder(OrderRequest request, StreamObserver<OrderReply> responseObserver) 
+     {
         Integer id = 2;
         OrderReply reply = OrderReply.newBuilder().setId(id).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
      }
 
-     public void GetOrders(Empty request, StreamObserver<ItemResponse> responseObserver) {
-        ItemResponse reply = ItemResponse.newBuilder().build();
+     
+     @Override
+     public void getOrders(Empty request, StreamObserver<ItemResponse> responseObserver) 
+     {
+         // TODO Auto-generated method stub
+         //super.getOrders(request, responseObserver);
+
+         var ordersResponse = new ArrayList<SalesOrderApi.OrderResponse>();
+         ordersResponse.add(OrderResponse.newBuilder().setId(2).build());
+
+         ItemResponse reply = ItemResponse.newBuilder().addAllItems(ordersResponse).build();
         
-        responseObserver.onNext(reply);
-        responseObserver.onCompleted();
+         responseObserver.onNext(reply);
+         responseObserver.onCompleted();
      }
+   
 
 }
